62fec2413ef9cb1b15a24bac2ff469fda62d89d9
Andrew <andrew.presland@gmail.com>
2018-03-04 15:40:49 +0100

    persist tweet stream rdd and save tweet metics to C*

--- commons/src/main/scala/ch/presland/data/domain/Sentiment.scala
+++
@@ -1,5 +0,0 @@
-package ch.presland.data.domain
-
-import java.util.Date
-
-case class Sentiment (id: String, time: Date, sentiment: Int)
---
+++ commons/src/main/scala/ch/presland/data/domain/TweetMetric.scala
@@ -0,0 +1,5 @@
+package ch.presland.data.domain
+
+import java.util.Date
+
+case class TweetMetric(id: String, time: Date, sentiment: Int)
