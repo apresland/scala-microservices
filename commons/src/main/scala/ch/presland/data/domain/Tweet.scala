fe0df49363ee45829b6ecde292b37961d3298d4e
Andrew <andrew.presland@gmail.com>
2018-03-18 17:37:53 +0100

    adds experimental hashtag counting (needs cleaning)

--- commons/src/main/scala/ch/presland/data/domain/Tweet.scala
+++ commons/src/main/scala/ch/presland/data/domain/Tweet.scala
@@ -5,3 +5,7 @@ import java.util.Date
 case class Tweet(id: String, time: Date, user: String, content: String)
 case class Sentiments( length: Int, dimension: Int, zero: Iterable[Double], one:Iterable[Double], two:Iterable[Double], three:Iterable[Double], four:Iterable[Double])
 
+case class Hashtags( length: Int, dimension: Int,
+                     zero: Iterable[Double], one:Iterable[Double], two:Iterable[Double], three:Iterable[Double], four:Iterable[Double],
+                     five: Iterable[Double], six:Iterable[Double], seven:Iterable[Double], eight:Iterable[Double], nine:Iterable[Double]
+                   )
