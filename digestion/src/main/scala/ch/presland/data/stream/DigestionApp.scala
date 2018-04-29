23139b5b093646c3d660c901e02a4708f5e54377
Andrew <andrew.presland@gmail.com>
2018-04-29 17:04:29 +0200

    Reduced complexity of DigestionApp

--- digestion/src/main/scala/ch/presland/data/stream/DigestionApp.scala
+++ digestion/src/main/scala/ch/presland/data/stream/DigestionApp.scala
@@ -16,20 +16,11 @@ import com.datastax.driver.core.utils._
 import scala.collection.immutable.Map
 import ch.presland.data.domain.Tweet
 
-import scala.collection.convert.wrapAll._
+
 import java.util.Date
 import java.time.{LocalDateTime, ZoneId}
 
-import akka.NotUsed
-import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, OverflowStrategy}
-import akka.stream.scaladsl._
-
-
 object DigestionApp extends App {
-
-  implicit val system = ActorSystem("reactive-tweets")
-  implicit val materializer = ActorMaterializer()
 
   val sparkConf = new SparkConf()
     .setAppName(getClass.getSimpleName)
@@ -60,65 +51,16 @@ object DigestionApp extends App {
     .map(consumerRecord => consumerRecord.value())
     .persist()
 
+  new SentimentAnalyser(ssc)
+  .analyse(tweets)
+
+  new HashtagAnalyser(ssc)
+    .analyse(tweets)
+
   tweets
     .saveToCassandra("twitter", "tweets")
-
-  // Tweet sentiment analysis
-  tweets
-    .map(tweet => TweetAnalyser.sentiment(tweet))
-    .map(sentiment => sentiment.score)
-    .window(Seconds(60))
-    .foreachRDD((rdd, time) => saveSentimentToCassandra(ssc, time,
-      rdd.aggregate(Map(0 -> 0L, 1 -> 0L, 2 -> 0L, 3 -> 0L))(SentimentAggregator.seqOp, SentimentAggregator.combOp))
-    )
-
-  // Tweet hashtag trending
-  tweets
-    .map(t => t.content)
-    .flatMap(text => text.toLowerCase().split(" ").filter(_.startsWith("#")))
-    .map((_, 1))
-    .reduceByKeyAndWindow(_ + _, Seconds(600))
-    .map { case (tag, count) => (tag, count) }
-    .transform(_.sortBy(p => p._2, ascending = false))
-    .foreachRDD(rdd => saveHashtagToCassandra(
-      ssc, rdd.take(10).aggregate(Map.empty[String, Long])(HashtagAggregator.seqOp, HashtagAggregator.combOp))
-    )
 
   ssc.start()
   ssc.awaitTermination()
 
-  def saveSentimentToCassandra(ssc: StreamingContext, time: org.apache.spark.streaming.Time, sentiment: Map[SentimentAggregator.Category,Long]): Unit = {
-
-    val date = LocalDateTime.now()
-    val timestamp = Date.from(date.atZone(ZoneId.systemDefault()).toInstant)
-    val row = Seq((timestamp, sentiment.get(0), sentiment.get(1), sentiment.get(2), sentiment.get(3)))
-
-    ssc.sparkContext.parallelize(row)
-      .saveToCassandra("twitter","sentiments", SomeColumns("time", "hostile", "negative", "neutral", "positive"))
-  }
-
-  def saveHashtagToCassandra(ssc: StreamingContext, hashtags: Map[String,Long]): Unit = {
-
-    val date = LocalDateTime.now()
-    val timestamp = Date.from(date.atZone(ZoneId.systemDefault()).toInstant)
-
-    val keys = hashtags.keySet.toList
-
-    val row = Seq((UUIDs.timeBased(), timestamp,
-      (keys.get(0), hashtags.get(keys.get(0))),
-      (keys.get(1), hashtags.get(keys.get(1))),
-      (keys.get(2), hashtags.get(keys.get(2))),
-      (keys.get(3), hashtags.get(keys.get(3))),
-      (keys.get(4), hashtags.get(keys.get(4))),
-      (keys.get(5), hashtags.get(keys.get(5))),
-      (keys.get(6), hashtags.get(keys.get(6))),
-      (keys.get(7), hashtags.get(keys.get(7))),
-      (keys.get(8), hashtags.get(keys.get(8))),
-      (keys.get(9), hashtags.get(keys.get(9)))
-    ))
-
-    ssc.sparkContext.parallelize(row)
-      .saveToCassandra("twitter","hashtags",
-        SomeColumns("id","time", "tag0", "tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "tag8", "tag9"))
-  }
 }
